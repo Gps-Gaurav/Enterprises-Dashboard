ae389771e6e15547a693b551966089b38918770e
Gaurav Pratap Singh <gauravpratapsingh321@gmail.com>
2025-08-23 00:53:26 +0530

    commit

--- Angular 14/src/environments/environment.prod.ts
+++ Angular 14/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://enterprises-dashboard-api.onrender.com'
-  apiUrl: 'http://127.0.0.1:8000'
+  apiUrl: 'https://enterprises-dashboard.onrender.com'
+  // apiUrl: 'http://127.0.0.1:8000'
   // apiUrl: 'http://127.0.0.1:8080'
 };
--- Angular 14/src/environments/environment.ts
+++ Angular 14/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   // apiUrl: 'https://enterprises-dashboard-api.onrender.com'
-  apiUrl: 'http://127.0.0.1:8000'  // Your development API URL
+  apiUrl: 'https://enterprises-dashboard.onrender.com'
+  // apiUrl: 'http://127.0.0.1:8000'  // Your development API URL
   // apiUrl: 'http://127.0.0.1:8080'  // Your development API URL
 };
